51a7dd104a08e5cd8bf845ff96fad26586cadfb2
brunoperetti <brunoperetti10@gmail.com>
2021-01-26 16:08:21 -0300

    Primer commit

---
+++ app/src/main/java/com/fgb/ventaya/UI/PantallaInicio.java
@@ -0,0 +1,17 @@
+package com.fgb.ventaya.UI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fgb.ventaya.R;
+
+public class PantallaInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_inicio);
+    }
+
+}
---
+++ app/src/main/java/com/fgb/ventaya/UI/Registro.java
@@ -0,0 +1,4 @@
+package com.fgb.ventaya.UI;
+
+public class Registro {
+}
